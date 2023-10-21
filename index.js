1dcc37e68df29eeadb2fcd6ff2c2dc8a7543d156
komed3 <webmaster@komed3.de>
2023-10-21 19:13:53 +0200

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * similarstr
+ * lightweight npm package to calculate string similarity
+ * 
+ * @author komed3 (Paul Köhler)
+ * @version 1.0.0
+ * @license MIT
+ */
+
+'use strict'
+
+module.exports.similarity = ( a, b ) => {
+
+    //
+
+};
