817a2081a6cb4e23f1c37e03099d770206fa128c
komed3 <pk97@outlook.de>
2025-04-05 01:02:07 +0200

    basic functionality (test / closest / match) and adding one algorithms

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 module.exports = class CmpStr {
 
-    register = [ 'levenshtein', 'dice' ];
+    #register = {
+        levenshtein: require( './algorithms/levenshtein' )
+    };
 
     string;
     flags = '';
     algo;
 
-    register ( algo ) {
+    addAlgo ( algo, callback, useIt = true ) {
 
-        //
+        if (
+            !this.isAlgo( algo ) &&
+            typeof callback === 'function' &&
+            callback.length == 2 &&
+            typeof callback.apply( null, [ 'abc', 'abc' ] ) === 'number'
+        ) {
+
+            this.#register[ algo ] = callback;
+
+            if ( useIt ) {
+
+                this.setAlgo( algo );
+
+            }
+
+        } else {
+
+            throw new Error ( 'the algorithm could not be added' );
+
+        }
 
     };
 
@@ -18,9 +39,9 @@ module.exports = class CmpStr {
 
     };
 
-    setAlgorithm ( algo ) {
+    setAlgo ( algo ) {
 
-        if ( this.isAlgorithm( algo ) ) {
+        if ( this.isAlgo( algo ) ) {
 
             this.algo = algo;
 
@@ -32,15 +53,79 @@ module.exports = class CmpStr {
 
     };
 
-    isAlgorithm ( algo ) {
+    isAlgo ( algo ) {
 
-        return this.register.includes( algo );
+        return algo in this.#register;
 
     };
 
     test ( str ) {
 
+        return this.#register[ this.algo ].apply(
+            null, [ this.string, String( str ) ]
+        );
 
+    };
+
+    closest ( arr ) {
+
+        let best = -Infinity,
+            idx = 0, pct;
+
+        /* search for closest element in arr */
+
+        [ ...arr ].forEach( ( str, i ) => {
+
+            pct = this.test( str );
+
+            if( pct > best ) {
+
+                /* save closest target */
+
+                best = pct;
+                idx = i;
+
+            }
+
+        } );
+
+        /* return closest target */
+
+        return arr[ idx ];
+
+    };
+
+    match ( arr, threshold = 0 ) {
+
+        let matches = [],
+            pct;
+
+        /* calculate similarity for each item in arr */
+
+        [ ...arr ].forEach( ( str ) => {
+
+            pct = this.test( str );
+
+            if( pct >= threshold ) {
+
+                matches.push( {
+                    target: str, match: pct,
+                    fromTh: pct - threshold
+                } );
+
+            }
+
+        } );
+
+        /* sort by highest similarity */
+
+        let sorted = matches.sort(
+            ( a, b ) => b.match - a.match
+        );
+
+        /* return sorted matches */
+
+        return sorted;
 
     };
 
