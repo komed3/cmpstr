b7aea573e45ee71c16d666642fea035d370c049c
komed3 <pk97@outlook.de>
2025-04-05 19:08:53 +0200

    add new methor compare and change lazy-loading modules

--- src/index.js
+++ src/index.js
@@ -40,20 +40,20 @@ module.exports = class CmpStr {
     #cache = new Map();
 
     /**
-     * current algorithm to use for similarity calculations
-     * set by setAlgo(), addAlgo() or constructor()
-     * 
-     * @type {String}
-     */
-    algo;
-
-    /**
      * base string for comparison
      * set by setStr or constructor()
      * 
      * @type {String}
      */
     str;
+
+    /**
+     * current algorithm to use for similarity calculations
+     * set by setAlgo(), addAlgo() or constructor()
+     * 
+     * @type {String}
+     */
+    algo;
 
     /**
      * initializes a CmpStr instance
@@ -79,6 +79,20 @@ module.exports = class CmpStr {
     };
 
     /**
+     * sets the base string for comparison
+     * 
+     * @param {String} str string to set as the base
+     * @returns {Boolean} always returns true
+     */
+    setStr ( str ) {
+
+        this.str = String( str );
+
+        return true;
+
+    };
+
+    /**
      * checks whether string and algorithm are set correctly
      * 
      * @returns {Boolean} is ready state
@@ -95,14 +109,44 @@ module.exports = class CmpStr {
     };
 
     /**
-     * checks if an algorithm is registered
+     * @private
+     * (save) lazy-load algorithm module
      * 
-     * @param {String} algo name of the algorithm to use for calculation
-     * @returns {Boolean} true if the algorithm is registered, false otherwise
+     * @param {String} algo name of the similarity algorithm
+     * @returns {Boolean} true if the algorithm is loaded
+     * @throws {Error} if the algorithm cannot be loaded or is not defined
      */
-    isAlgo ( algo ) {
+    #loadAlgo( algo ) {
 
-        return algo in this.#algorithms;
+        if ( this.isAlgo( algo ) ) {
+
+            let typeOf = typeof this.#algorithms[ algo ];
+
+            if ( typeOf === 'function' ) {
+
+                return true;
+
+            } else if ( typeOf === 'string' ) {
+
+                /* lazy load modules */
+
+                this.#algorithms[ algo ] = require(
+                    this.#algorithms[ algo ]
+                );
+
+                return true;
+
+            } else {
+
+                throw new Error ( algo + ' cannot be loaded' );
+
+            }
+
+        } else {
+
+            throw new Error ( algo + ' is not defined' );
+
+        }
 
     };
 
@@ -118,9 +162,21 @@ module.exports = class CmpStr {
     };
 
     /**
-     * adds a new similarity algorithm to the class
+     * checks if an algorithm is registered
      * 
      * @param {String} algo name of the algorithm to use for calculation
+     * @returns {Boolean} true if the algorithm is registered, false otherwise
+     */
+    isAlgo ( algo ) {
+
+        return algo in this.#algorithms;
+
+    };
+
+    /**
+     * adds a new similarity algorithm to the class
+     * 
+     * @param {String} algo name of the algorithm
      * @param {Function} callback function implementing the algorithm (must accept two strings and return a number)
      * @param {Boolean} [useIt=true] whether to set this algorithm as the current one
      * @returns {Boolean} returns true if the algorithms was added successfully
@@ -156,7 +212,7 @@ module.exports = class CmpStr {
     /**
      * removes a similarity algorithm from the class
      * 
-     * @param {String} algo name of the algorithm to remove
+     * @param {String} algo name of the algorithm
      * @returns {Boolean} true if the algorithm was removed successfully
      * @throws {Error} if the algorithm is not defined
      */
@@ -189,58 +245,32 @@ module.exports = class CmpStr {
      * 
      * @param {String} algo name of the algorithm to use for calculation
      * @returns {Boolean} true if the algorithm has been set
-     * @throws {Error} if the algorithm is not defined
      */
     setAlgo ( algo ) {
 
-        if ( this.isAlgo( algo ) ) {
-
-            if ( typeof this.#algorithms[ algo ] === 'string' ) {
-
-                /* lazy load modules */
-
-                this.#algorithms[ algo ] = require( this.#algorithms[ algo ] );
-
-            }
+        if ( this.#loadAlgo( algo ) ) {
 
             this.algo = algo;
 
             return true;
-
-        } else {
-
-            throw new Error ( algo + ' is not defined' );
 
         }
 
     };
 
     /**
-     * sets the base string for comparison
-     * 
-     * @param {String} str string to set as the base
-     * @returns {Boolean} always returns true
-     */
-    setStr ( str ) {
-
-        this.str = String( str );
-
-        return true;
-
-    };
-
-    /**
      * @private
-     * normalize a string
-     * 
-     * @param {String} string to normalize
-     * @param {String} [flags=''] flags for normalization
+     * normalize a string, options can be chained;
+     * list of all supported flags:
      * - case insensitivity (i)
      * - trim whitespaces (t)
      * - remove special chars (s)
      * - normalize unicode (u)
      * - ignore numbers (n)
      * - collapse whitespaces (w)
+     * 
+     * @param {String} string to normalize
+     * @param {String} [flags=''] flags for normalization
      * @returns {String} normalized string
      */
     #normalize ( str, flags = '' ) {
@@ -305,10 +335,33 @@ module.exports = class CmpStr {
 
     };
 
-    compare ( a, b, flags = '', ...args ) {};
+    /**
+     * compares two string a and b using the given algorithm
+     * 
+     * @param {String} algo name of the algorithm
+     * @param {String} a string a
+     * @param {String} b string b
+     * @param {String} flags [flags=''] flags for normalization
+     * @param {...any} args additional arguments to pass to the algorithm
+     * @returns {Number} similarity score between 0 and 1
+     */
+    compare ( algo, a, b, flags = '', ...args ) {
+
+        if ( this.#loadAlgo( algo ) ) {
+
+            return this.#algorithms[ algo ].apply( null, [
+                this.#normalize( a, flags ),
+                this.#normalize( b, flags ),
+                ...args
+            ] );
+
+        }
+
+    };
 
     /**
-     * tests the similarity between the base string and a target string using the current algorithm
+     * tests the similarity between the base string and a target string
+     * using the current algorithm
      * 
      * @param {String} str target string to compare
      * @param {String} [flags=''] flags for normalization
@@ -319,11 +372,11 @@ module.exports = class CmpStr {
 
         if ( this.isReady() ) {
 
-            return this.#algorithms[ this.algo ].apply( null, [
-                this.#normalize( this.str, flags ),
-                this.#normalize( str, flags ),
-                ...args
-            ] );
+            return this.compare(
+                this.algo,
+                this.str, str,
+                flags, ...args
+            );
 
         }
 
@@ -401,23 +454,21 @@ module.exports = class CmpStr {
     /**
      * generate a similarity matrix for an array of strings
      * 
-     * @param {String} algo name of the algorithm to use for calculation
-     * @param {String[]} arr array of strings to compare
+     * @param {String} algo name of the algorithm
+     * @param {String[]} arr array of strings to cross-compare
      * @param {String} [flags=''] flags for normalization
      * @param {...any} args additional arguments to pass to the algorithm
      * @returns {Number[][]} 2D array representing the similarity matrix
      */
     similarityMatrix ( algo, arr, flags = '', ...args ) {
 
-        if ( this.setAlgo( algo ) ) {
+        if ( this.#loadAlgo( algo ) ) {
 
             return [ ...arr ].map( ( a, i ) => {
 
-                this.setStr( a );
-
                 return [ ...arr ].map(
-                    ( b, j ) => i === j ? 1 : this.test(
-                        b, flags, ...args
+                    ( b, j ) => i === j ? 1 : this.compare(
+                        algo, a, b, flags, ...args
                     )
                 );
 
