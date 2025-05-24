cfaf973359b1fece842716c7f06d4f141a1b5498
komed3 <pk97@outlook.de>
2025-05-24 11:35:30 +0200

    add docs + check for empty strings

--- src/metrics/Levensthein.ts
+++ src/metrics/Levensthein.ts
@@ -1,54 +1,89 @@
+/**
+ * Levenshtein Distance
+ * src/metrics/Levensthein.ts
+ * 
+ * The Levenshtein distance between two strings is the minimum number of
+ * single-character edits (i.e. insertions, deletions or substitutions)
+ * required to change one word into the other.
+ * 
+ * @see https://en.wikipedia.org/wiki/Levenshtein_distance
+ * 
+ * @author Paul Köhler (komed3)
+ * @license MIT
+ * @since 1.0.0
+ */
+
 'use strict';
 
 import type { MetricResult } from '../utils/Types.js';
 
+/**
+ * Calculate the Levenshtein distance between two strings.
+ * 
+ * @param a - First string.
+ * @param b - Second string.
+ * @returns MetricResult containing the metric name, input strings, raw distance, and similarity result.
+ */
 export default (
-    a : string,
-    b : string
+    a: string,
+    b: string
 ) : MetricResult => {
 
-    /** Use always the shorter string as columns (save memory) */
-    [ a, b ] = a.length > b.length ? [ b, a ] : [ a, b ];
+    let m : number = a.length;
+    let n : number = b.length;
+    const maxLen : number = Math.max( m, n );
 
-    const m = a.length;
-    const n = b.length;
-    const maxLen = Math.max( m, n );
+    let raw : number;
 
-    let prev = new Array ( m + 1 );
-    let curr = new Array ( m + 1 );
+    // Check for empty strings
+    if ( a === b ) raw = 0;
+    else if ( m === 0 ) raw = n;
+    else if ( n === 0 ) raw = m;
 
-    /** Initialization of the first line */
-    for ( let i = 0; i <= m; i++ ) prev[ i ] = i;
+    else {
 
-    /** Loop through the characters of the second string */
-    for ( let j = 1; j <= n; j++ ) {
+        // Use always the shorter string as columns (save memory)
+        [ a, b, m, n ] = m > n ? [ b, a, n, m ] : [ a, b, m, n ];
 
-        curr[ 0 ] = j;
+        let prev : number[] = new Array ( m + 1 );
+        let curr : number[] = new Array ( m + 1 );
 
-        for ( let i = 1; i <= m; i++ ) {
+        // Initialization of the first line
+        for ( let i = 0; i <= m; i++ ) prev[ i ] = i;
 
-            const cost = a[ i - 1 ] === b[ j - 1 ] ? 0 : 1;
+        // Loop through the characters of the second string
+        for ( let j = 1; j <= n; j++ ) {
 
-            curr[ i ] = Math.min(
-                curr[ i - 1 ] + 1,    // insert
-                prev[ i ] + 1,        // delete
-                prev[ i - 1 ] + cost  // replace
-            );
+            curr[ 0 ] = j;
+
+            for ( let i = 1; i <= m; i++ ) {
+
+                const cost : number = a[ i - 1 ] === b[ j - 1 ] ? 0 : 1;
+
+                curr[ i ] = Math.min(
+                    curr[ i - 1 ] + 1,    // insertion
+                    prev[ i ] + 1,        // deletion
+                    prev[ i - 1 ] + cost  // replacement
+                );
+
+            }
+
+            // Swap the lines
+            [ prev, curr ] = [ curr, prev ];
 
         }
 
-        /** Swap the lines */
-        [ prev, curr ] = [ curr, prev ];
+        raw = prev[ m ];
 
     }
 
-    /** Calculate string similarity */
-    const res = maxLen === 0 ? 1 : 1 - prev[ m ] / maxLen;
+    // Calculate string similarity
+    const res = maxLen === 0 ? 1 : 1 - raw / maxLen;
 
-    /** Return the result */
+    // Return the result
     return {
-        metric: 'levensthein', a, b,
-        raw: prev[ m ], res
+        metric: 'levensthein',
+        a, b, raw, res
     };
 
 };
