951daaf5928f740bbbe309006b4c079c12be5581
komed3 <pk97@outlook.de>
2025-06-03 12:47:59 +0200

    implement static filter methods and profiler integration in CmpStr API

--- src/CmpStr.ts
+++ src/CmpStr.ts
@@ -11,4 +11,18 @@ import { TextAnalyzer } from './utils/TextAnalyzer';
 import { METRICS } from './metric/index';
 import { PHONETICS } from './phonetic/index';
 
-export default class CmpStr {};
+export default class CmpStr {
+
+    public static addFilter = Filter.add;
+    public static removeFilter = Filter.remove;
+    public static pauseFilter = Filter.pause;
+    public static resumeFilter = Filter.resume;
+    public static listFilter = Filter.list;
+    public static clearFilter = Filter.clear;
+
+    protected static readonly profiler = Profiler.getInstance();
+
+    public static profilerReport = this.profiler.getAll;
+    public static profilerClear = this.profiler.clear;
+
+};
