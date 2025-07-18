e7868848877fb63282ade4e060aa8c88877c78c1
komed3 <pk97@outlook.de>
2025-07-18 09:18:29 +0200

    some minor changes + vitest description

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,21 @@
+/**
+ * Vitest Configuration for CmpStr
+ * vitest.config.ts
+ *
+ * This configuration file sets up Vitest for testing the CmpStr library.
+ * 
+ * While running on GitHub Actions, it uses the `tap-flat` reporter for compact output and
+ * the `github-actions` reporter for integration with GitHub's test reporting features.
+ * 
+ * In local development, it defaults to the `verbose` reporter, which provides detailed
+ * output of test results.
+ * 
+ * @author Paul Köhler (komed3)
+ * @license MIT
+ */
+
+'use strict';
+
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig( {
