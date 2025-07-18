220d290769a954c2204fe65a13f363617e4da935
komed3 <pk97@outlook.de>
2025-07-18 12:00:09 +0200

    apply specific Caverphone rules

--- src/phonetic/Caverphone.ts
+++ src/phonetic/Caverphone.ts
@@ -35,6 +35,33 @@ export class Caverphone extends Phonetic {
      */
     constructor ( opt: PhoneticOptions = {} ) { super ( 'caverphone', opt ) }
 
+    /**
+     * Generates the Caverphone code for a given word.
+     * 
+     * @param {string} word - The input word to be converted into a Caverphone code
+     * @returns {string} - The generated Caverphone code
+     */
+    protected override encode ( word: string ) : string {
+
+        // Remove anything not A-Z and convert to lowercase
+        word = word.replace( /[^A-Z]/gi, '' ).toLowerCase();
+
+        // Apply specific Caverphone rules
+        for ( const [ s, r ] of [
+            [ /^cough/, 'cou2f' ], [ /^rough/, 'rou2f' ],
+            [ /^tough/, 'tou2f' ], [ /^enough/, 'enou2f' ],
+            [ /^gn/, '2n' ], [ /mb$/, 'm2' ]
+        ] as [ RegExp, string ][] ) {
+
+            word = word.replace( s, r );
+
+        }
+
+        // Use the base implementation for rule/mapping application
+        return super.encode( word );
+
+    }
+
 }
 
 // Register the Caverphone algorithm in the phonetic registry
@@ -43,7 +70,9 @@ PhoneticRegistry.add( 'caverphone', Caverphone );
 // Register the Caverphone 1.0 phonetic mapping for English
 PhoneticMappingRegistry.add( 'caverphone', 'en1', {
     map: {},
-    ruleset: []
+    ruleset: [
+        { char: 'c', next: [ 'q' ], code: '2q' }
+    ]
 } );
 
 // Register the Caverphone 2.0 phonetic mapping for English
