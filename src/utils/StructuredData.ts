28b3355bb5ffe89591198252850719295939a782
komed3 <pk97@outlook.de>
2026-01-17 00:51:06 +0100

    implement StructuredData factory method

--- src/utils/StructuredData.ts
+++ src/utils/StructuredData.ts
@@ -1,3 +1,13 @@
 import { MetricRaw } from './Types';
 
-export class StructuredData<T = any, R = MetricRaw> {}
+export class StructuredData<T = any, R = MetricRaw> {
+
+    public static create<T = any, R = MetricRaw> (
+        data: T[], key: string | number | symbol
+    ) : StructuredData<T, R> {
+
+        return new StructuredData ( data, key );
+
+    }
+
+}
