aef4868d3e995f27a8a3f54c629e22b0aa37c70a
komed3 <pk97@outlook.de>
2026-01-19 20:13:58 +0100

    fix IndexedResult type

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -239,11 +239,11 @@ export type MetricResultBatch<R = MetricRaw> = MetricResultSingle<R>[];
 export type MetricResult<R = MetricRaw> = MetricResultSingle<R> | MetricResultBatch<R>;
 
 /**
- * IndexedResults is an array of MetricResultSingle with optional index metadata.
+ * IndexedResult is an MetricResultSingle with optional index metadata.
  * 
  * @template R - The type of the raw result
  */
-export type IndexedResults<R = MetricRaw> = ( MetricResultSingle<R> & { __idx?: number } )[];
+export type IndexedResult<R = MetricRaw> = MetricResultSingle<R> & { __idx?: number };
 
 /**
  * PhoneticOptions configures the behavior of phonetic algorithms.
