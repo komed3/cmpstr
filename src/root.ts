a140f52cd623d39b53a218c1102c4c41801be0fd
komed3 <pk97@outlook.de>
2026-01-19 22:49:47 +0100

    bump vers. to 3.1.1

--- src/root.ts
+++ src/root.ts
@@ -9,7 +9,7 @@
  * Please visit CmpStr's documentation for more information:
  * https://github.com/komed3/cmpstr/wiki/Extending-CmpStr
  * 
- * @version 3.1.0
+ * @version 3.1.1
  * @author Paul Köhler (komed3)
  * @license MIT
  */
