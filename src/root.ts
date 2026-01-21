6db86ce349f0f54aacba782366ab1592d1ea169b
komed3 <pk97@outlook.de>
2026-01-21 19:53:25 +0100

    update vers. number

--- src/root.ts
+++ src/root.ts
@@ -9,7 +9,7 @@
  * Please visit CmpStr's documentation for more information:
  * https://github.com/komed3/cmpstr/wiki/Extending-CmpStr
  * 
- * @version 3.1.1
+ * @version 3.2.0
  * @author Paul Köhler (komed3)
  * @license MIT
  */
