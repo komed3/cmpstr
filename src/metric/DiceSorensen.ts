a82f7d46ff343587c6837e6528591372026023f7
komed3 <pk97@outlook.de>
2026-01-21 02:28:13 +0100

    Update DiceSorensen.ts

--- src/metric/DiceSorensen.ts
+++ src/metric/DiceSorensen.ts
@@ -33,7 +33,7 @@ export interface DiceRaw {
 /**
  * DiceSorensenCoefficient class extends the Metric class to implement the Dice-Sørensen coefficient.
  */
-export class DiceSorensenCoefficient extends Metric<DiceRaw> {
+export class DiceSorensenCoefficient extends Metric< DiceRaw > {
 
     /**
      * Constructor for the DiceSorensen class.
@@ -41,34 +41,29 @@ export class DiceSorensenCoefficient extends Metric<DiceRaw> {
      * Initializes the DiceSorensen metric with two input strings or
      * arrays of strings and optional options.
      * 
+     * Metric is symmetrical.
+     * 
      * @param {MetricInput} a - First input string or array of strings
      * @param {MetricInput} b - Second input string or array of strings
      * @param {MetricOptions} [opt] - Options for the metric computation
      */
     constructor ( a: MetricInput, b: MetricInput, opt: MetricOptions = {} ) {
-
-        // Call the parent Metric constructor with the metric name and inputs
-        // Metric is symmetrical
         super ( 'dice', a, b, opt, true );
-
     }
 
     /**
      * Computes the bigrams of a given string.
      * 
      * @param {string} str - The input string
-     * @return {Set<string>} - A set of bigrams (two-character sequences) from the string
+     * @return {Set< string >} - A set of bigrams (two-character sequences) from the string
      */
-    private _bigrams ( str: string ) : Set<string> {
-
-        const len: number = str.length - 1;
-        const bigrams: Set<string> = Pool.acquire( 'set', len );
+    private _bigrams ( str: string ) : Set< string > {
+        const len = str.length - 1;
+        const bigrams = Pool.acquire< Set< string > >( 'set', len );
 
         // Generate bigrams by iterating through the string
         for ( let i = 0; i < len; i++ ) bigrams.add( str.substring( i, i + 2 ) );
-
         return bigrams;
-
     }
 
     /**
@@ -76,33 +71,31 @@ export class DiceSorensenCoefficient extends Metric<DiceRaw> {
      * 
      * @param {string} a - First string
      * @param {string} b - Second string
-     * @return {MetricCompute<DiceRaw>} - Object containing the similarity result and raw distance
+     * @return {MetricCompute< DiceRaw >} - Object containing the similarity result and raw distance
      */
-    protected override compute ( a: string, b: string ) : MetricCompute<DiceRaw> {
-
+    protected override compute ( a: string, b: string ) : MetricCompute< DiceRaw > {
         // Generate bigrams for both strings
-        const setA: Set<string> = this._bigrams( a );
-        const setB: Set<string> = this._bigrams( b );
+        const setA = this._bigrams( a ), setB = this._bigrams( b );
+        const sizeA = setA.size, sizeB = setB.size;
 
-        // Calculate the intersection of bigrams
-        let intersection: number = 0;
+        try {
+            // Calculate the intersection of bigrams
+            let intersection = 0;
+            for ( const bigram of setA ) if ( setB.has( bigram ) ) intersection++;
 
-        for ( const bigram of setA ) if ( setB.has( bigram ) ) intersection++;
+            // Calculate the size of the union of both sets
+            const size = sizeA + sizeB;
 
-        // Calculate the size of the union of both sets
-        const sizeA: number = setA.size, sizeB: number = setB.size;
-        const size: number = sizeA + sizeB;
-
-        // Release sets back to the pool
-        Pool.release( 'set', setA, sizeA );
-        Pool.release( 'set', setB, sizeB );
-
-        // Return the result as a MetricCompute object
-        return {
-            res: size === 0 ? 1 : Metric.clamp( ( 2 * intersection ) / size ),
-            raw: { intersection, size }
-        };
-
+            // Return the result as a MetricCompute object
+            return {
+                res: size === 0 ? 1 : Metric.clamp( ( 2 * intersection ) / size ),
+                raw: { intersection, size }
+            };
+        } finally {
+            // Release sets back to the pool
+            Pool.release( 'set', setA, sizeA );
+            Pool.release( 'set', setB, sizeB );
+        }
     }
 
 }
