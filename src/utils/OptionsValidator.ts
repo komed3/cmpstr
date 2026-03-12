33a593cfe2d98f9f934d5e3cf9ade928f651176a
komed3 <pk97@outlook.de>
2026-03-12 09:57:54 +0100

    describe allowed flags / output modes

--- src/utils/OptionsValidator.ts
+++ src/utils/OptionsValidator.ts
@@ -1,3 +1,16 @@
 'use strict';
 
-export class OptionsValidator {}
+/**
+ * Utility for validating CmpStr options.
+ * 
+ * The validator is designed to be lightweight and safe to call frequently.
+ */
+export class OptionsValidator {
+
+    // Allowed normalization flags
+    private static readonly ALLOWED_FLAGS = new Set( [ 'd', 'u', 'x', 'w', 't', 'r', 's', 'k', 'n', 'i' ] );
+
+    // Allowed output modes
+    private static readonly ALLOWED_OUTPUT = new Set( [ 'orig', 'prep' ] );
+
+}
