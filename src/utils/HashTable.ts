26cdb512836f9567087ea5da5138c22b5e1c0b6c
komed3 <pk97@outlook.de>
2026-03-14 17:53:04 +0100

    Murmur finalizer uses Math.imul()

--- src/utils/HashTable.ts
+++ src/utils/HashTable.ts
@@ -68,9 +68,9 @@ export class Hasher {
 
         // Final mixing to improve distribution
         hash ^= hash >>> 16;
-        hash *= 0x85ebca6b;
+        hash = Math.imul( hash, 0x85ebca6b );
         hash ^= hash >>> 13;
-        hash *= 0xc2b2ae35;
+        hash = Math.imul( hash, 0xc2b2ae35 );
         hash ^= hash >>> 16;
 
         // Convert to unsigned 32-bit integer
